68f24109e679a526ddf3084787a402807b45c9b5
3laina <philpotter@vcu.edu>
2017-04-11 10:03:11 -0400

    test wsgi config

--- ReptileDB/wsgi.py
+++ ReptileDB/wsgi.py
@@ -11,6 +11,12 @@ import os
 
 import django.core.handlers.wsgi
 
+if path not in sys.path:
+    sys.path.append(path)
+
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ReptileDB.settings")
 
-application = django.core.handlers.wsgi.WSGIHandler()
